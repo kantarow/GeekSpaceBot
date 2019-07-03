4aa730ebc7a61bef5d7f6d9704fc5d5f255f7f3c
Lazialize <mistio100@gmail.com>
2019-07-03 13:35:03 +0900

    [Update] 全体的な見直し
    データ構造から誤りがあったため、全体的に書き直しを行いました。

--- extensions/vcrole.py
+++ extensions/vcrole.py
@@ -1,10 +1,12 @@
 import discord
+import logging
 
 from discord.ext import commands
 from gsbot import GSBot
 
 
 CONFIGPATH = 'config/vcrole.json'
+logger = logging.getLogger('gsbot.vcrole')
 
 
 class VCRole(commands.Cog):
@@ -17,34 +19,77 @@ class VCRole(commands.Cog):
         # TODO: リンクしたVCと役職のリストを表示
         pass
 
+    @vcrole.command(name='list')
+    async def list_vcrole(self, ctx, vc: discord.VoiceChannel):
+        settings = self.config.get(str(vc.id))
+
+        if settings is None:
+            await ctx.send('指定のボイスチャンネルにリンクされている役職は存在しません。')
+            return
+
+        embeds = list()
+        embed = discord.Embed()
+        count = 1
+        for data in settings:
+            guild = self.bot.get_guild(data['guild_id'])
+            role = guild.get_role(data['role_id'])
+
+            embed.add_field(
+                name=str(count),
+                value='Guild: {0}\nRole: {1}'.format(guild.name, role.name),
+                inline=False,
+            )
+
+            count += 1
+
+            # フィールドの数が20個になったら、新しいEmbedを作成する。
+            if count % 20 == 0:
+                embeds.append(embed)
+                embed = discord.Embed()
+        else:
+            embeds.append(embed)
+
+        for embed in embeds:
+            await ctx.send(embed=embed)
+
     @vcrole.command(name='add')
     async def add_vcrole(self, ctx, vc: discord.VoiceChannel, role: discord.Role):
         # TODO: VCと役職の紐付けを追加する
-        setting = self.config.get(str(vc.id))
-        if setting is None:
-            setting = [role.id]
-        else:
-            setting.append(role.id)
+        settings = self.config.get(str(vc.id))
+        if settings is None:
+            settings = list()
 
-        self.config[str(vc.id)] = setting
+            data = dict()
+            data['role_id'] = role.id
+            data['guild_id'] = role.guild.id
+
+            settings.append(data)
+        else:
+            data = dict()
+            data['role_id'] = role.id
+            data['guild_id'] = role.guild.id
+
+            settings.append(data)
+
+        self.config[str(vc.id)] = settings
         self.bot.save_config(CONFIGPATH, self.config)
 
     @vcrole.command(name='remove')
-    async def remove_vcrole(self, ctx, vc: discord.VoiceChannel, role: discord.Role):
+    async def remove_vcrole(self, ctx, vc: discord.VoiceChannel, index: int):
         # TODO: VCと役職の紐付けを削除する
-        setting = self.config.get(str(vc.id))
+        settings = self.config.get(str(vc.id))
 
-        if setting is None:
+        if settings is None:
             await ctx.send('指定のボイスチャンネルには役職が設定されていません。')
             return
 
         try:
-            setting.remove(role.id)
-        except ValueError:
-            await ctx.send('指定のボイスチャンネルには指定の役職が設定されていません。')
+            settings.pop(index - 1)
+        except IndexError:
+            await ctx.send('指定されたインデックスは範囲を超えています。')
             return
 
-        self.config[str(vc.id)] = setting
+        self.config[str(vc.id)] = settings
         self.bot.save_config(CONFIGPATH, self.config)
 
     @commands.Cog.listener()
@@ -54,7 +99,41 @@ class VCRole(commands.Cog):
         before: discord.VoiceState,
         after: discord.VoiceState,
     ):
-        pass
+        if before.channel is None:
+            self.check_and_add_roles()
+
+        elif after.channel is not None:
+            self.check_and_remove_roles()
+            self.check_and_add_roles()
+
+    async def check_and_add_roles(
+        self, member: discord.Member, channel: discord.VoiceChannel
+    ):
+        settings = self.config.get(str(channel.id))
+
+        if settings is None:
+            return
+
+        for data in settings:
+            guild = self.bot.get_guild(data['guild_id'])
+            if guild is None:
+                logger.warning('Guild is not found. ID: {0}'.format(data['guild_id']))
+                continue
+
+            role = guild.get_role(data['role_id'])
+            if role is None:
+                logger.warning('Role is not found. ID: {0}'.format(data['role_id']))
+                continue
+
+        await member.add_roles()
+
+    async def check_and_remove_roles(
+        self, member: discord.Member, channel: discord.VoiceChannel
+    ):
+        setting = self.config.get(str(channel.id))
+
+        if setting is None:
+            return
 
 
 def setup(bot):
