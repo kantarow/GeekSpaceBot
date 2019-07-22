badb6242c21d1d6f03c273a016abc4f4394f8086
Lazialize <mistio100@gmail.com>
2019-07-22 09:49:47 +0900

    [Fix] 小さなバグの修正
    Botアカウントの初期化以前に実行されてしまうために、タスクが終了するバグを修正。

--- extensions/gnupdate.py
+++ extensions/gnupdate.py
@@ -12,6 +12,7 @@ class GuildNameUpdater(commands.Cog):
 
     @tasks.loop(hours=1)
     async def name_update(self):
+        await self.bot.wait_until_ready()
         guild = self.bot.get_guild(406357894427312148)
         await guild.edit(name='Geek-Space {0}'.format(guild.member_count))
 
