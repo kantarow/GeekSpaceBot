5d8d1638994c6cae9bf280c784a704a44294629c
Lazialize <mistio100@gmail.com>
2019-12-01 22:35:37 +0900

    [Hotfix] await及びasyncのつけ忘れを修正

--- extensions/guildname_updater.py
+++ extensions/guildname_updater.py
@@ -11,16 +11,16 @@ class GuildNameUpdater(commands.Cog):
         if member.guild.id != 406357894427312148:
             return
 
-        self.change_guild_name()
+        await self.change_guild_name()
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
         if member.guild.id != 406357894427312148:
             return
 
-        self.change_guild_name()
+        await self.change_guild_name()
 
-    def change_guild_name(self):
+    async def change_guild_name(self):
         guild = self.bot.get_guild(406357894427312148)
         await guild.edit(name="Geek-Space +{0} members".format(guild.member_count))
 
